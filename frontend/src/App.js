736498c94412d43eb5cd113e68b839b73ccd1cbf
Andrew <45044183+andoos@users.noreply.github.com>
2021-01-17 16:19:38 -0500

    modified project structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,23 @@
-import './App.css';
-import React, {Component} from 'react';
-import styled from 'styled-components';
-import LoginScreen from './LoginScreen/LoginScreen';
+import "./App.css";
+import React from "react";
+import UpdateButton from "./UpdateButton/UpdateButton";
+import LogoutButton from "./LogoutButton/LogoutButton";
+import LoginScreen from "./LoginScreen/LoginScreen";
 
-const Button = styled.button`
-  background-color: rgb(88, 85, 95);
-  color: white;
-  padding: 5px 15px;
-  border-radius: 5px;
-  outline: 0;
-  cursor: pointer;
-  margin: 0px 5px;
-  &:hover {
-    background-color: rgb(92, 89, 99);
-  }
-`;
-
-class App extends Component {
-  state = {
-    loginCredentials: {username: 'username', password: 'password'},
+function App() {
+  /*const state = {
+    loginCredentials: { username: "username", password: "password" },
     isAuthenticated: false,
-  }
-
-  render() {
-    return (
-      <div className = 'App'>
-        {/* <h1>NMK02: Contact Tracing Wristband with Temperature Sensor and Cloud-based Monitoring Dashboard</h1>
-        <Button onClick = {update}>
-          Refresh
-        </Button>
-        <Button onClick = {logout}>
-          logout
-        </Button> */}
-        <LoginScreen credentials={this.state.loginCredentials}></LoginScreen>
-      </div>
-    );
-  }
-}
-
-function update(){
-  alert('updated.');
-}
-
-function logout(){
-  alert('logout successful.');
-}
-
-/*function App() {
+  };*/
   return (
     <div className="App">
-      <p>hello</p>
+      {/* <LoginScreen credentials={this.state.loginCredentials}></LoginScreen> */}
+      <div>
+        <h1>Administrator's Dashboard</h1>
+        <UpdateButton /> <LogoutButton />
+      </div>
     </div>
   );
-}*/
+}
 
 export default App;
