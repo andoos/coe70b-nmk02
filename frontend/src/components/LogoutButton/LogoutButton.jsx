fd1524edcd7a566d5cc04ee7fecc45f4fd5df1df
EraOfTruth <33705236+EraOfTruth@users.noreply.github.com>
2021-01-25 21:15:01 -0500

    routing code added (#4)

--- frontend/src/components/LogoutButton/LogoutButton.jsx
+++ frontend/src/components/LogoutButton/LogoutButton.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Button } from "reactstrap";
 
 import LogoutStyles from "./LogoutButton.module.css";
 
-const doLogout = () => {
-  console.log("The log out button was clicked.");
-  alert("logout successful.");
-};
+import { Switch, Redirect } from "react-router-dom";
 
 function LogoutButton() {
+  const [isLogout, setIsLogout] = useState(false);
+
+  const doLogout = () => {
+    console.log("The log out button was clicked.");
+    alert("logout successful.");
+    setIsLogout(true);
+    <Switch>
+      <Redirect from="/admin-dashboard" to="/"></Redirect>
+    </Switch>;
+  };
+  
   return (
     <div className={LogoutStyles.LogoutButton}>
       <Button className="btn-round btn-icon" color="info" onClick={doLogout}>
         <i className="tim-icons icon-upload" />
       </Button>
+      { isLogout?
+        <Switch>
+          <Redirect from="/admin-dashboard" to="/"></Redirect>
+        </Switch> :
+        null
+      }
     </div>
   );
 }
