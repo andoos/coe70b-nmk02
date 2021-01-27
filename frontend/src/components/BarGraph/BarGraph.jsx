df330e318a728df120c090ac7b1b8b54ef5f646b
omar-ahmad <omarahmad1998@gmail.com>
2021-01-26 23:22:27 -0500

    Added props to DataTable, cleaned charts.js (#6)

--- frontend/src/components/BarGraph/BarGraph.jsx
+++ frontend/src/components/BarGraph/BarGraph.jsx
@@ -4,20 +4,17 @@ import { Bar } from "react-chartjs-2";
 import { Card, CardBody, CardHeader, CardTitle, Container } from "reactstrap";
 
 import {
-  chartExample1,
-  chartExample2,
   chartExample3,
-  chartExample4,
 } from "./charts.js";
 
 function BarGraph() {
   return (
     <Card className="card-chart">
       <CardHeader>
-        <h5 className="card-category">Daily Sales</h5>
-        <CardTitle tag="h3">
+        <h5 className="card-category">Sum of Employees not Social Distancing</h5>
+        {/* <CardTitle tag="h3">
           <i className="tim-icons icon-delivery-fast text-primary" /> 3,500€
-        </CardTitle>
+        </CardTitle> */}
       </CardHeader>
       <CardBody>
         <div className="chart-area">
--- frontend/src/views/AdminDashboard.jsx
+++ frontend/src/views/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container, Row, Col, CardDeck, CardColumns } from "reactstrap";
 
@@ -11,28 +11,38 @@ import BarGraph from "../components/BarGraph/BarGraph.jsx";
 //import NucleoIcons from "./assets/css/nucleo-icons.css";
 
 function AdminDashboard() {
+  const [table, setTable] = useState([
+    { title: "Employees not Social Distancing", header1: "Employee 1", header2: "Employee 2", header3: "Distance (m)", header4: "Duration (mins)"},
+    { title: "Employees with High Temperatures", data: ""},
+    { title: "Employees to be sent home", data: ""},    
+  ])
+
+
   return (
     <Container fluid>
       <AdminNavBar></AdminNavBar>
       <ul></ul>
       <Row>
         <Col>
-          <DataTable />
+          <DataTable title = "Employees not Social Distancing" header1 = "Employee 1" header2 = "Employee 2" header3 = "Distance (m)" header4 = "Duration (mins)" data = "bluetoothData"/>
+          {/* {table.map(tbl => (
+            <DataTable title = {tbl.title} data = {tbl.data}/>
+          ))} */}
         </Col>
         <Col>
-          <DataTable />
+          <DataTable title = "Employees with High Temperatures" header1 = "Employee" header2 = "Temperature (Celsius)" header3 = "Shift Start" header4 = "Shift End" data = "temperatureData"/>
         </Col>
         <Col>
-          <DataTable />
+          <DataTable title = "Employees to be sent home" header1 = "Employee" header2 = "FLAG" header3 = "Shift Start" header4 = "Shift End" data = "flagData"/>
         </Col>
       </Row>
       <Row>
         <Col>
           <BarGraph />
         </Col>
-        <Col>
+        {/* <Col>
           <BarGraph />
-        </Col>
+        </Col> */}
       </Row>
     </Container>
   );
