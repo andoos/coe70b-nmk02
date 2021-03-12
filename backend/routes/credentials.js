6c301d9adc7c56f9b6ca6a74724039f5577c560a
Raymond <rayraythai@gmail.com>
2021-03-12 17:16:34 -0500

    Added code for credentials query

--- backend/routes/credentials.js
+++ backend/routes/credentials.js
@@ -1,9 +1,27 @@
 var express = require("express");
+var mysql = require("mysql");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+const db = mysql.createConnection({
+  host: "nmk02-mysql-test.csqhhjgbuho8.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "Omar1234",
+  database: "hardware",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected");
+});
+
+/* GET temperature readings for a wrist id*/
+router.get("/usercredentials", function (req, res, next) {
+  const sqlQuery = "SELECT * FROM UserCredentials';";
+  db.query(sqlQuery, (err, result) => {
+    console.log(result);
+    res.send(result);
+  });
+  //res.render("index", { title: "Express" });
 });
 
 module.exports = router;
