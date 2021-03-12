f082cfcad84c9f33ae3c9c29fb939bcc449f2dd1
Andrew <andrew98alli@gmail.com>
2021-03-12 18:02:03 -0500

    move db conn to new file

--- backend/routes/credentials.js
+++ backend/routes/credentials.js
@@ -1,22 +1,11 @@
 var express = require("express");
-var mysql = require("mysql");
 var router = express.Router();
 
-const db = mysql.createConnection({
-  host: "nmk02-mysql-test.csqhhjgbuho8.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "Omar1234",
-  database: "hardware",
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Connected");
-});
+const { db } = require("../database.js");
 
 /* GET user credentials stored in user credentials table*/
-router.get("/usercredentials", function (req, res, next) {
-  const sqlQuery = "SELECT * FROM UserCredentials';";
+router.get("/", function (req, res, next) {
+  const sqlQuery = "SELECT * FROM UserCredentials;";
   db.query(sqlQuery, (err, result) => {
     console.log(result);
     res.send(result);
