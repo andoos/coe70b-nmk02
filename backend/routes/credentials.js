4921d30bd71c2b97e6bfbfedde1b415e4f6e6928
Raymond <rayraythai@gmail.com>
2021-03-12 18:22:12 -0500

    Added authentication code

--- backend/routes/credentials.js
+++ backend/routes/credentials.js
@@ -15,11 +15,14 @@ db.connect((err) => {
 });
 
 /* GET user credentials stored in user credentials table*/
-router.get("/usercredentials", function (req, res, next) {
-  const sqlQuery = "SELECT * FROM UserCredentials';";
+router.get("/", function (req, res, next) {
+  const sqlQuery = "SELECT * FROM hardware.UserCredentials WHERE Id = 1;";
   db.query(sqlQuery, (err, result) => {
     console.log(result);
-    res.send(result);
+    res.send({
+      username: result[0].Username,
+      password: result[0].Password,
+    });
   });
   //res.render("index", { title: "Express" });
 });
