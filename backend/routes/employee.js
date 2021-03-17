da5316874d8e49182611ea6e1ecfc0661127ecb3
Andrew <andrew98alli@gmail.com>
2021-03-17 16:59:04 -0400

    added query params to employees api

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -4,12 +4,24 @@ var router = express.Router();
 
 const { db } = require("../database.js");
 
-/* GET all employee readings */
+/* GET all employee readings, query paramters to get readings for a day */
 router.get("/", cors(), function (req, res, next) {
-  const sqlQuery = "SELECT * FROM hardware.Employees";
-  db.query(sqlQuery, (err, result) => {
-    res.send(result);
-  });
+  if (req.query.startTime == null && req.query.endTime == null) {
+    const sqlQuery = "SELECT * FROM hardware.Employees";
+    db.query(sqlQuery, (err, result) => {
+      res.send(result);
+    });
+  } else {
+    const sqlQuery =
+      "SELECT * FROM hardware.Employees WHERE ShiftStart >= " +
+      req.query.startTime +
+      " AND ShiftEnd <= " +
+      req.query.endTime +
+      ";";
+    db.query(sqlQuery, (err, result) => {
+      res.send(result);
+    });
+  }
 });
 
 /* GET employee name attached to a wrist ID, query parameter time to get employee name */
