b77bc4b5e62e5c0681e0b5469ed6052d6a2011a4
Andrew <andrew98alli@gmail.com>
2021-03-16 22:50:17 -0400

    added query params to bluetooth api

--- backend/routes/bluetooth.js
+++ backend/routes/bluetooth.js
@@ -1,12 +1,21 @@
 var express = require("express");
 var router = express.Router();
-var cors = require('cors');
+var cors = require("cors");
 
 const { db } = require("../database.js");
 
 /* GET all bluetooth readings*/
 router.get("/", cors(), function (req, res, next) {
-  const sqlQuery = "SELECT * FROM hardware.BluetoothEvent;";
+  if (req.query.startTime == null && req.query.endTime == null) {
+    sqlQuery = "SELECT * FROM hardware.BluetoothEvent;";
+  } else {
+    sqlQuery =
+      "SELECT * FROM hardware.BluetoothEvent WHERE Timestamp > " +
+      req.query.startTime +
+      " AND Timestamp < " +
+      req.query.endTime +
+      ";";
+  }
   db.query(sqlQuery, (err, result) => {
     res.send(result);
   });
@@ -14,11 +23,12 @@ router.get("/", cors(), function (req, res, next) {
 
 /* GET sum of all bluetooth readings for the hour */
 router.get("/sum", cors(), function (req, res, next) {
-  const sqlQuery = "SELECT HOUR(Timestamp) 'hr', COUNT(Timestamp) 'count' FROM hardware.BluetoothEvent WHERE DAY(Timestamp) = DAY(NOW()) GROUP BY hr;";
+  const sqlQuery =
+    "SELECT HOUR(Timestamp) 'hr', COUNT(Timestamp) 'count' FROM hardware.BluetoothEvent WHERE DAY(Timestamp) = DAY(NOW()) GROUP BY hr;";
   db.query(sqlQuery, (err, result) => {
-    res.send(result)
-  })
-})
+    res.send(result);
+  });
+});
 
 /* GET all bluetooth readings for a wrist id*/
 router.get("/:wristId", function (req, res, next) {
