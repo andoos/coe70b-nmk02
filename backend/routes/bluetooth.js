6bff5e2e232508dca6bdef68dae4566c81f64795
Andrew <andrew98alli@gmail.com>
2021-03-17 21:50:27 -0400

    bluetooth api returns name instead of id

--- backend/routes/bluetooth.js
+++ backend/routes/bluetooth.js
@@ -3,18 +3,33 @@ var router = express.Router();
 var cors = require("cors");
 
 const { db } = require("../database.js");
+const { request } = require("express");
 
 /* GET all bluetooth readings*/
 router.get("/", cors(), function (req, res, next) {
   if (req.query.startTime == null && req.query.endTime == null) {
     sqlQuery = "SELECT * FROM hardware.BluetoothEvent;";
   } else {
+    // sqlQuery =
+    //   "SELECT * FROM hardware.BluetoothEvent WHERE Timestamp > " +
+    //   req.query.startTime +
+    //   " AND Timestamp < " +
+    //   req.query.endTime +
+    //   ";";
     sqlQuery =
-      "SELECT * FROM hardware.BluetoothEvent WHERE Timestamp > " +
+      "SELECT t1.BluetoothEventID, t2.EmployeeName AS EmployeeA, t3.EMployeeName AS EmployeeB, t1.Distance, t1.Timestamp FROM (SELECT * FROM hardware.BluetoothEvent WHERE Timestamp > " +
       req.query.startTime +
       " AND Timestamp < " +
       req.query.endTime +
-      ";";
+      ") t1 INNER JOIN (SELECT * FROM hardware.Employees WHERE ShiftStart >= " +
+      req.query.startTime +
+      " AND ShiftEnd <= " +
+      req.query.endTime +
+      ") t2 ON t1.Wrist_ID_A = t2.Wrist_ID INNER JOIN (SELECT * FROM hardware.Employees WHERE ShiftStart >= " +
+      req.query.startTime +
+      " AND ShiftEnd <= " +
+      req.query.endTime +
+      ") t3 ON t1.Wrist_ID_B = t3.Wrist_ID;";
   }
   db.query(sqlQuery, (err, result) => {
     res.send(
@@ -22,8 +37,8 @@ router.get("/", cors(), function (req, res, next) {
         return {
           BluetoothEventID: element.BluetoothEventID,
           Distance: element.Distance,
-          Wrist_ID_A: element.Wrist_ID_A,
-          Wrist_ID_B: element.Wrist_ID_B,
+          EmployeeA: element.EmployeeA,
+          EmployeeB: element.EmployeeB,
           Timestamp: new Date(element.Timestamp * 1000).toLocaleString(),
         };
       })
