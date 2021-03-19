956f85c08879e444f2353e6a8764aa7a32aa6096
Jacob Pasqualoni <jacob.pasqualoni@gmail.com>
2021-03-19 04:32:54 -0400

    Hardware script for test cases complete; lambda function comments and RSSI conversion implementation; mockdata script updated with new MAC addresses for the bluetooth modules

--- hardware/LambdaFunction/IOTtoRDSLambdaFunction.py
+++ hardware/LambdaFunction/IOTtoRDSLambdaFunction.py
@@ -1,21 +1,29 @@
 import pymysql
 import json
 
+
+# distance conversion
+# RSSI = -20log(r)+R0
+# r = 10^[(RSSI-R0)/-20]
+# r is distance in cm RSSI is value given by bluetooth reading and R0 is a DC gain adjustment
+# 65483 seems like a good value for R0 *shrug*
+# FFC5 corrosponds to 2m distance
+
 def handler(event, context):
     connection = pymysql.connect(host='nmk02-mysql-test.csqhhjgbuho8.us-east-2.rds.amazonaws.com', user='admin',
                                  password='Omar1234', db='hardware', connect_timeout=31536)
-    
+
     if event['type'] == 'TemperatureEvent':
         with connection:
             with connection.cursor() as cursor:
                 sql = "INSERT INTO hardware.TemperatureEvent VALUES ('', '" + event["temperature"] + "', '" + event["deviceID"] + "', '" + event["time"] + "');"
                 cursor.execute(sql)
                 connection.commit()
-                
+
     if event['type'] == 'BluetoothEvent':
+        # str(10**((65483-(int(event['distance'], 16)))/20))
         with connection:
             with connection.cursor() as cursor:
-                sql = "INSERT INTO hardware.BluetoothEvent VALUES ('', '" + event['distance'] + "', '" + event['deviceIDA'] + "', '" + event['deviceIDB'] + "', '" + event['time'] + "');"
+                sql = "INSERT INTO hardware.BluetoothEvent VALUES ('', '" + str(10 ** ((65483 - (int(event['distance'], 16))) / 20)) + "', '" + event['deviceIDA'] + "', '" + event['deviceIDB'] + "', '" + event['time'] + "');"
                 cursor.execute(sql)
                 connection.commit()
-        
