b316568569bc260a5274d65aaf7d860f2b0c8adc
Andrew <andrew98alli@gmail.com>
2021-04-07 20:39:11 -0400

    imported css from module

--- frontend/src/components/DateFilter/DateFilter.jsx
+++ frontend/src/components/DateFilter/DateFilter.jsx
@@ -1,15 +1,26 @@
 import React, { useState, forwardRef } from "react";
 
+import { Button, UncontrolledTooltip } from "reactstrap";
+
 import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+
 import DateFilterStyles from "./DateFilter.module.css";
+
+// icon-calendar-60
 
 function DateFilter(props) {
   const [startDate, setStartDate] = useState(new Date());
   const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
-    <button className="example-custom-input" onClick={onClick} ref={ref}>
-      {value}
-    </button>
+    <Button
+      className="btn-round btn-icon"
+      color="info"
+      onClick={onClick}
+      ref={ref}
+    >
+      <i className="tim-icons icon-calendar-60" />
+    </Button>
   ));
   return (
     <div className={DateFilterStyles.DateFilter}>
@@ -20,6 +31,12 @@ function DateFilter(props) {
       />
     </div>
   );
+}
+
+{
+  /* <button className="example-custom-input" onClick={onClick} ref={ref}>
+{value}
+</button> */
 }
 
 export default DateFilter;
