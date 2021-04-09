b59050b2c6bf513251a7eb07b8b6644441997783
Andrew <andrew98alli@gmail.com>
2021-04-09 12:59:37 -0400

    Made timestamp generation easier to configure in mock data script

--- hardware/scripts/mock_data.py
+++ hardware/scripts/mock_data.py
@@ -1,7 +1,7 @@
-import random
 import time
-import pymysql
 import datetime
+import random
+import pymysql
 
 from random import randrange
 
@@ -9,24 +9,24 @@ wrist_id = ["84:CC:A8:A4:41:79",
             "84:CC:A8:DA:32:EC", 
             "84:CC:A8:51:C8:AD", 
             "84:CC:A8:64:65:DC", 
-            "84:CC:A8:EA:1B:78", 
+            "84:CC:A8:EA:1B:78",
+            "84-CC-A8-8B-3E-17", 
             "00:20:09:00:05:02", # small breadboard (actual prototype) 
-            "00:20:08:00:1A:C1"  # big breadboard (actual prototype) 
-]
+            "00:20:08:00:1A:C1"] # big breadboard (actual prototype) 
 
 # generate a bluetooth event 
-BTdeviceA = wrist_id[randrange(6)]
-BTdeviceB = wrist_id[randrange(6)]
+BTdeviceA = wrist_id[randrange(len(wrist_id))]
+BTdeviceB = wrist_id[randrange(len(wrist_id))]
 
 # make sure device IDs are not the same
 while BTdeviceA == BTdeviceB:
-        BTdeviceB = wrist_id[randrange(6)]        
+        BTdeviceB = wrist_id[randrange(len(wrist_id))]
 
 distance = str(round(random.uniform(0, 2), 1))
-# time = str(random.randrange(1615813200, 1615856400)) # March 15 2021 9:00 
-# time = str(random.randrange(1615899600, 1615942800)) # March 16 2021 9:00 
-# time = str(random.randrange(1616072400, 1616115600)) # March 18 2021 9:00 - 21:00
-time = str(random.randrange(1616158800, 1616202000)) # March 19 2021 9:00 - 21:00
+
+startTime = round(time.mktime(datetime.datetime(2021, 4, 9, 9, 0).timetuple()))
+endTime = round(time.mktime(datetime.datetime(2021, 4, 9, 21, 0).timetuple()))
+timestamp = str(random.randrange(startTime, endTime))
 
 # generate a temperature event for each user 
 temperature_A = str(round(random.uniform(36.0, 41.0), 1))
@@ -36,14 +36,14 @@ connection = pymysql.connect(host='nmk02-mysql-test.csqhhjgbuho8.us-east-2.rds.a
 
 with connection: 
         with connection.cursor() as cursor:        
-                sql = "INSERT INTO hardware.BluetoothEvent (Distance, Wrist_ID_A, Wrist_ID_B, Timestamp) VALUES ('" + distance + "', '" + BTdeviceA + "', '" + BTdeviceB + "', '" + time + "');"
+                sql = "INSERT INTO hardware.BluetoothEvent (Distance, Wrist_ID_A, Wrist_ID_B, Timestamp) VALUES ('" + distance + "', '" + BTdeviceA + "', '" + BTdeviceB + "', '" + timestamp + "');"
                 cursor.execute(sql)
                 connection.commit()
 
-                sql = "INSERT INTO hardware.TemperatureEvent (Temperature, Wrist_ID, Timestamp) VALUES ('" + temperature_A + "', '" + BTdeviceA + "', '" + time + "');"
+                sql = "INSERT INTO hardware.TemperatureEvent (Temperature, Wrist_ID, Timestamp) VALUES ('" + temperature_A + "', '" + BTdeviceA + "', '" + timestamp + "');"
                 cursor.execute(sql)
                 connection.commit()
 
-                sql = "INSERT INTO hardware.TemperatureEvent (Temperature, Wrist_ID, Timestamp) VALUES ('" + temperature_B + "', '" + BTdeviceB + "', '" + time + "');"
+                sql = "INSERT INTO hardware.TemperatureEvent (Temperature, Wrist_ID, Timestamp) VALUES ('" + temperature_B + "', '" + BTdeviceB + "', '" + timestamp + "');"
                 cursor.execute(sql)
                 connection.commit()
